2a3c53cb5247dcc807163d1c0a476b4d1d55ad48
Abdullah Foqha <abdullahfoqha@abdullas-mbp.mynetworksettings.com>
2023-08-06 22:27:15 -0400

    [Lab-8]: add solution

---
+++ index.js
@@ -0,0 +1,51 @@
+let gameStarted = false;
+
+let boundary = $('.boundary');
+$('.won').hide();
+$('.lost').hide();
+
+$(document).ready(() => {
+  $('#start').on('click', () => {
+    gameStarted = true;
+    $('#status').html('Started');
+    boundary.removeClass('lose-status');
+    $('.lost').hide();
+    $('.won').hide();
+  });
+
+  $('.boundary').on('mouseenter', () => {
+    if (gameStarted) {
+      $('#status').html('Click the "S" to begin.');
+      gameStarted = false;
+      boundary.addClass('lose-status');
+      $('.lost').show();
+
+      setTimeout(() => {
+        alert('You Lost!!');
+      }, 50);
+    }
+  });
+
+  $('#maze').on('mouseleave', () => {
+    if (gameStarted) {
+      $('#status').html('Click the "S" to begin.');
+      gameStarted = false;
+      boundary.addClass('lose-status');
+      $('.lost').show();
+      setTimeout(() => {
+        alert('You Lost!!');
+      }, 50);
+    }
+  });
+
+  $('#end').on('mouseenter', () => {
+    if (gameStarted) {
+      $('#status').html('Click the "S" to begin.');
+      gameStarted = false;
+      $('.won').show();
+      setTimeout(() => {
+        alert('You Won');
+      }, 50);
+    }
+  });
+});
